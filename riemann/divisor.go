bf2368811cb7b757116371c1e8d19690acf1a65a
alexsanjoseph <alexsanjoseph@gmail.com>
2021-12-26 18:41:22 +0530

    Added implementation for divisor tests

--- riemann/divisor.go
+++ riemann/divisor.go
@@ -1,5 +1,16 @@
 package riemann
 
-func DivisorSum(n int) int {
-	return 0
+import "errors"
+
+func DivisorSum(n int) (int, error) {
+	if n <= 0 {
+		return 0, errors.New("value cannot be less than or equal to zero")
+	}
+	sum := n
+	for i := 1; i < n; i++ {
+		if n%i == 0 {
+			sum += i
+		}
+	}
+	return sum, nil
 }
--- riemann/divisor_test.go
+++ riemann/divisor_test.go
@@ -1,16 +1,43 @@
 package riemann_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/alexsanjoseph/riemann-divisor-sum-go/riemann"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func TestDivisorSum(t *testing.T) {
-	sum := riemann.DivisorSum(192)
-	fmt.Println(sum)
-	if sum != 72 {
-		t.Fatal("Divisor sum doesn't match!")
-	}
+func TestDivisor(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Divisor")
 }
+
+var _ = Describe("Divisor", func() {
+
+	Context("Good Cases", func() {
+		It("Should give correct result", func() {
+			output, _ := riemann.DivisorSum(72)
+			Expect(output).To(Equal(195))
+		})
+
+		It("Should give correct result", func() {
+			output, _ := riemann.DivisorSum(1)
+			Expect(output).To(Equal(1))
+		})
+	})
+
+	Context("Bad Cases", func() {
+		It("zero value", func() {
+			output, err := riemann.DivisorSum(0)
+			Expect(output).To(BeZero())
+			Expect(err).To(HaveOccurred())
+		})
+
+		It("negative value", func() {
+			output, err := riemann.DivisorSum(-5)
+			Expect(output).To(BeZero())
+			Expect(err).To(HaveOccurred())
+		})
+	})
+})
