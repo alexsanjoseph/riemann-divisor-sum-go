57d60a2647f99025ab8634d79992634e5deb5530
alexsanjoseph <alexsanjoseph@gmail.com>
2021-12-26 18:33:58 +0530

    Made tests an external package

--- riemann/divisor_test.go
+++ riemann/divisor_test.go
@@ -1,12 +1,14 @@
-package riemann
+package riemann_test
 
 import (
 	"fmt"
 	"testing"
+
+	"github.com/alexsanjoseph/riemann-divisor-sum-go/riemann"
 )
 
 func TestDivisorSum(t *testing.T) {
-	sum := DivisorSum(192)
+	sum := riemann.DivisorSum(192)
 	fmt.Println(sum)
 	if sum != 72 {
 		t.Fatal("Divisor sum doesn't match!")
