df012302d8001396a688c7da6b58127c4cfab48b
alexsanjoseph <alexsanjoseph@gmail.com>
2021-12-26 19:13:53 +0530

    Updated Divisor Sum to use Euler Implementation

--- riemann/divisor.go
+++ riemann/divisor.go
@@ -1,8 +1,12 @@
 package riemann
 
-import "errors"
+import (
+	"errors"
+)
 
-func DivisorSum(n int) (int, error) {
+var DIVISOR_CACHE = make(map[int]int)
+
+func DivisorSumNaive(n int) (int, error) {
 	if n <= 0 {
 		return 0, errors.New("value cannot be less than or equal to zero")
 	}
@@ -12,5 +16,54 @@ func DivisorSum(n int) (int, error) {
 			sum += i
 		}
 	}
+	return sum, nil
+}
+
+func divisorSumCached(n int) (int, error) {
+
+	result, ok := DIVISOR_CACHE[n]
+
+	if ok {
+		return result, nil
+	}
+
+	sum, error := DivisorSum(n)
+	DIVISOR_CACHE[n] = sum
+	return sum, error
+}
+
+func DivisorSum(n int) (int, error) {
+	if n <= 0 {
+		return 0, errors.New("value cannot be less than or equal to zero")
+	}
+
+	signSequence := []int{1, 1, -1, -1}
+
+	sum := 0
+
+	for i := 0; true; i++ {
+		sign := signSequence[i%4]
+		x := i/2 + 1
+
+		if i%2 == 1 {
+			x = -x
+		}
+		argDiff := int((3*x*x - x) / 2)
+
+		if n-argDiff < 0 {
+			break
+		}
+
+		if n-argDiff == 0 {
+			sum += sign * n
+			break
+		}
+		output, err := divisorSumCached(n - argDiff)
+		if err != nil {
+			panic("Error shouldn't come here!")
+		}
+		sum += sign * output
+	}
+
 	return sum, nil
 }
