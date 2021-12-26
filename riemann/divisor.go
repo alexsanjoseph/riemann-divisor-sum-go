d918f89302a51f037634617c1115f2c1a78f0ada
alexsanjoseph <alexsanjoseph@gmail.com>
2021-12-26 19:14:54 +0530

    Updated Divisor Sum to use Square Root implementation

--- riemann/divisor.go
+++ riemann/divisor.go
@@ -2,6 +2,7 @@ package riemann
 
 import (
 	"errors"
+	"math"
 )
 
 var DIVISOR_CACHE = make(map[int]int)
@@ -32,7 +33,7 @@ func divisorSumCached(n int) (int, error) {
 	return sum, error
 }
 
-func DivisorSum(n int) (int, error) {
+func DivisorSumEuler(n int) (int, error) {
 	if n <= 0 {
 		return 0, errors.New("value cannot be less than or equal to zero")
 	}
@@ -63,6 +64,27 @@ func DivisorSum(n int) (int, error) {
 			panic("Error shouldn't come here!")
 		}
 		sum += sign * output
+	}
+
+	return sum, nil
+}
+
+func DivisorSum(n int) (int, error) {
+	if n <= 0 {
+		return 0, errors.New("value cannot be less than or equal to zero")
+	}
+
+	limit := math.Sqrt(float64(n))
+	sum := 0
+
+	for i := 1; float64(i) <= limit; i++ {
+		if n%i == 0 {
+			sum += i
+			if float64(i) != limit {
+				sum += n / i
+			}
+		}
+
 	}
 
 	return sum, nil
