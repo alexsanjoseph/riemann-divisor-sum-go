9f61164a2f987fbb6c0076a65134e224a3e91c0b
alexsanjoseph <alexsanjoseph@gmail.com>
2022-01-09 18:02:53 +0530

    Add Optional start and end points for search

--- main.go
+++ main.go
@@ -24,8 +24,7 @@ func main() {
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt, syscall.SIGINT)
 	var db = riemann.DivisorDb(riemann.InMemoryDivisorDb{Data: make(map[int64]riemann.RiemannDivisorSum)})
-	go riemann.PopulateDB(db, 1000000)
-
+	go riemann.PopulateDB(db, 10081, -1, 1000000)
 	<-sigCh
 	handleClose(db)
 }
