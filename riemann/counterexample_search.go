faeb0a109264eff6132d630802cbfdfc4317895c
alexsanjoseph <alexsanjoseph@gmail.com>
2022-01-09 16:26:25 +0530

    Added Precomputer Witness value as an optimization

--- riemann/counterexample_search.go
+++ riemann/counterexample_search.go
@@ -5,19 +5,25 @@ import (
 	"math"
 )
 
-func WitnessValue(n int) float64 {
+func WitnessValue(n int, pds int) float64 {
 	denom := float64(n) * math.Log(math.Log(float64(n)))
-
-	divSum, err := DivisorSum(n)
+	var divSum int
+	var err error
+	if pds < 0 {
+		divSum, err = DivisorSum(n)
+	} else {
+		divSum, err = pds, nil
+	}
 	if err != nil {
 		panic("Error calculating DivisorSum")
 	}
+
 	return float64(divSum) / float64(denom)
 }
 
 func Search(maxRange, searchStart int) (int, error) {
 	for i := searchStart; i < maxRange; i++ {
-		if WitnessValue(i) > 1.782 {
+		if WitnessValue(i, -1) > 1.782 {
 			return i, nil
 		}
 	}
@@ -28,7 +34,7 @@ func BestWitness(maxRange, searchStart int) (int, float64) {
 	maxVal := 0.0
 	bestWitness := searchStart
 	for i := searchStart; i < maxRange; i++ {
-		currentWitness := WitnessValue(i)
+		currentWitness := WitnessValue(i, -1)
 		if currentWitness > maxVal {
 			bestWitness = i
 			maxVal = currentWitness
