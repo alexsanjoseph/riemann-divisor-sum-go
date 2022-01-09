237fe79ebb6ea8b32b7440eea0059dbd90c1f497
alexsanjoseph <alexsanjoseph@gmail.com>
2022-01-09 18:05:21 +0530

    Removed some debugging code

--- riemann/populate_database_test.go
+++ riemann/populate_database_test.go
@@ -1,8 +1,6 @@
 package riemann_test
 
 import (
-	"fmt"
-
 	"github.com/alexsanjoseph/riemann-divisor-sum-go/riemann"
 
 	. "github.com/onsi/ginkgo"
@@ -11,14 +9,10 @@ import (
 
 var _ = Describe("Populates Database correctly", func() {
 
-	for i := 0; i < 10; i++ {
-		It("Populates and Summarizes correctly", func() {
-			var db = riemann.DivisorDb(riemann.InMemoryDivisorDb{Data: make(map[int64]riemann.RiemannDivisorSum)})
-			riemann.PopulateDB(db, 10070, 10085, 11)
-			var loadedData = db.Load()
-			fmt.Println(loadedData)
-			summaryData := db.Summarize()
-			Expect(summaryData.LargestWitnessValue.N).To(Equal(int64(10080)))
-		})
-	}
+	It("Populates and Summarizes correctly", func() {
+		var db = riemann.DivisorDb(riemann.InMemoryDivisorDb{Data: make(map[int64]riemann.RiemannDivisorSum)})
+		riemann.PopulateDB(db, 10070, 10085, 21)
+		summaryData := db.Summarize()
+		Expect(summaryData.LargestWitnessValue.N).To(Equal(int64(10080)))
+	})
 })
