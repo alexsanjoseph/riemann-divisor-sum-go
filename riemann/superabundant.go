2c9c28375de723a2b606db953c3f8982089cafd3
alexsanjoseph <alexsanjoseph@gmail.com>
2022-10-05 18:47:08 +0530

    Memoized Partitions

--- riemann/superabundant.go
+++ riemann/superabundant.go
@@ -15,3 +15,30 @@ func PartitionsOfN(n int) [][]int {
 	}
 	return output
 }
+
+func cachedPartitionsOfN(n int, cache map[int][][]int) [][]int {
+	possibleOutput, ok := cache[n]
+	if ok {
+		return possibleOutput
+	}
+
+	output := [][]int{{n}}
+	arrayToAdd := make([]int, n)
+	splitPoint := 0
+	arrayToAdd[splitPoint] = n
+	for i := n - 1; i > 0; i-- {
+		newParts := cachedPartitionsOfN(n-i, cache)
+		cache[n-i] = newParts
+		for _, part := range newParts {
+			if part[0] <= i {
+				output = append(output, append([]int{i}, part...))
+			}
+		}
+	}
+	return output
+}
+
+func MemoizedPartitionsOfN(n int) [][]int {
+	cache := make(map[int][][]int)
+	return cachedPartitionsOfN(n, cache)
+}
