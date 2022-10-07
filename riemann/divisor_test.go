a90a5df060d3580a5fb77d6ad9222467bbcdab64
alexsanjoseph <alexsanjoseph@gmail.com>
2022-10-08 01:49:09 +0530

    Cleaned up divisor test

--- riemann/divisor_test.go
+++ riemann/divisor_test.go
@@ -6,56 +6,57 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Divisor", func() {
+var _ = Describe("Divisor Calculation", func() {
 
-	Describe("Good Cases", func() {
-		It("Should give correct result", func() {
-			output, err := riemann.DivisorSum(72)
-			if err != nil {
-				Fail("error should be nil")
-			}
-			Expect(output).To(Equal(int64(195)))
-		})
-
-		It("Should give correct result", func() {
-			output, err := riemann.DivisorSum(1)
-			if err != nil {
-				Fail("error should be nil")
-			}
-			Expect(output).To(Equal(int64(1)))
-		})
-	})
-
-	Context("Bad Cases", func() {
-		It("zero value", func() {
-			output, err := riemann.DivisorSum(0)
-			Expect(output).To(BeZero())
-			Expect(err).To(HaveOccurred())
-		})
-
-		It("negative value", func() {
-			output, err := riemann.DivisorSum(-5)
-			Expect(output).To(BeZero())
-			Expect(err).To(HaveOccurred())
-		})
-	})
-
-	It("Should work on parametrized cases", func() {
-		divisorSums := []int64{
-			1, 3, 4, 7, 6, 12, 8, 15, 13, 18, 12, 28, 14, 24, 24, 31, 18, 39, 20, 42, 32,
-			36, 24, 60, 31, 42, 40, 56, 30, 72, 32, 63, 48, 54, 48, 91, 38, 60, 56, 90, 42,
-			96, 44, 84, 78, 72, 48, 124, 57, 93, 72,
-		}
-
-		for i, sum := range divisorSums {
-			By("Passing each cases", func() {
-				output, err := riemann.DivisorSum(int64(i) + 1)
+	Describe("Naive Divisor sum", func() {
+		Context("should work on good cases", func() {
+			It("Should give correct result", func() {
+				output, err := riemann.DivisorSum(72)
 				if err != nil {
 					Fail("error should be nil")
 				}
-				Expect(output).To(Equal(sum))
+				Expect(output).To(Equal(int64(195)))
 			})
-		}
 
+			It("Should give correct result", func() {
+				output, err := riemann.DivisorSum(1)
+				if err != nil {
+					Fail("error should be nil")
+				}
+				Expect(output).To(Equal(int64(1)))
+			})
+		})
+
+		Context("should error out on bad cases", func() {
+			It("zero value", func() {
+				output, err := riemann.DivisorSum(0)
+				Expect(output).To(BeZero())
+				Expect(err).To(HaveOccurred())
+			})
+
+			It("negative value", func() {
+				output, err := riemann.DivisorSum(-5)
+				Expect(output).To(BeZero())
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		It("should work on parametrized cases", func() {
+			divisorSums := []int64{
+				1, 3, 4, 7, 6, 12, 8, 15, 13, 18, 12, 28, 14, 24, 24, 31, 18, 39, 20, 42, 32,
+				36, 24, 60, 31, 42, 40, 56, 30, 72, 32, 63, 48, 54, 48, 91, 38, 60, 56, 90, 42,
+				96, 44, 84, 78, 72, 48, 124, 57, 93, 72,
+			}
+
+			for i, sum := range divisorSums {
+				By("Passing each cases", func() {
+					output, err := riemann.DivisorSum(int64(i) + 1)
+					if err != nil {
+						Fail("error should be nil")
+					}
+					Expect(output).To(Equal(sum))
+				})
+			}
+		})
 	})
 })
