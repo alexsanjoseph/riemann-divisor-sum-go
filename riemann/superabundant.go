bd1f673ec2bb01bf2a9eca6186e33ad24ab03656
alexsanjoseph <alexsanjoseph@gmail.com>
2022-10-07 23:50:31 +0530

    Added Kun style partition function

--- riemann/superabundant.go
+++ riemann/superabundant.go
@@ -42,3 +42,43 @@ func MemoizedPartitionsOfN(n int) [][]int {
 	cache := make(map[int][][]int)
 	return cachedPartitionsOfN(n, cache)
 }
+
+func PartitionsOfKun(n int) [][]int {
+	arrayToAdd := make([]int, n)
+	splitPoint := 0
+	output := [][]int{}
+	arrayToAdd[splitPoint] = n
+
+	for {
+		newArray := []int{}
+		for _, piece := range arrayToAdd {
+			if piece != 0 {
+				newArray = append(newArray, piece)
+			}
+		}
+		output = append(output, newArray)
+
+		rightOfNonOne := 0
+
+		for splitPoint >= 0 && arrayToAdd[splitPoint] == 1 {
+			rightOfNonOne += 1
+			splitPoint -= 1
+		}
+
+		if splitPoint < 0 {
+			break
+		}
+
+		arrayToAdd[splitPoint] -= 1
+		amountToSplit := rightOfNonOne + 1
+
+		for amountToSplit > arrayToAdd[splitPoint] {
+			arrayToAdd[splitPoint+1] = arrayToAdd[splitPoint]
+			amountToSplit -= arrayToAdd[splitPoint]
+			splitPoint += 1
+		}
+		arrayToAdd[splitPoint+1] = amountToSplit
+		splitPoint += 1
+	}
+	return output
+}
