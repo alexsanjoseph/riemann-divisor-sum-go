7ac0ad6cabca8a4f15b026e2a3d22f5f9e9280ba
alexsanjoseph <alexsanjoseph@gmail.com>
2022-10-10 01:40:25 +0530

    Big Int Divisor Sum

--- riemann/divisor.go
+++ riemann/divisor.go
@@ -6,6 +6,7 @@ import (
 	"math/big"
 )
 
+var ErrCheckFailed = errors.New("array not unique or prime")
 var ErrIntegerOverflow = errors.New("integer overflow")
 
 func DivisorSum(n int64) (int64, error) {
@@ -32,20 +33,20 @@ func DivisorSum(n int64) (int64, error) {
 	return sum, nil
 }
 
-func CheckIfPrime(n int) bool {
-	return big.NewInt(int64(n)).ProbablyPrime(0)
+func CheckIfPrime(n int64) bool {
+	return big.NewInt(n).ProbablyPrime(0)
 }
 
-func CheckUniqueness(n []int) bool {
-	allValsSet := make(map[int]struct{})
+func CheckUniqueness(n []int64) bool {
+	allValsSet := make(map[int64]struct{})
 	for _, x := range n {
 		allValsSet[x] = struct{}{}
 	}
 	return len(allValsSet) == len(n)
 }
 
-func checkUniquePrimeFactors(PrimeFactors [][]int) bool {
-	baseArray := []int{}
+func checkUniquePrimeFactors(PrimeFactors [][]int64) bool {
+	baseArray := []int64{}
 	for _, x := range PrimeFactors {
 		baseArray = append(baseArray, x[0])
 		isPrime := CheckIfPrime(x[0])
@@ -57,24 +58,49 @@ func checkUniquePrimeFactors(PrimeFactors [][]int) bool {
 	return CheckUniqueness(baseArray)
 }
 
-func PrimeFactorDivisorSum(PrimeFactors [][]int) (int64, error) {
+func PrimeFactorDivisorSum(PrimeFactors [][]int64) (big.Int, error) {
 
 	if len(PrimeFactors) < 1 {
-		return 1, nil
+		return *big.NewInt(1), nil
 	}
 
 	checksPassed := checkUniquePrimeFactors(PrimeFactors)
 	if !checksPassed {
-		return -1, ErrIntegerOverflow
+		return *big.NewInt(0), ErrCheckFailed
 	}
 
-	divisorSum := int64(1)
+	// divisorSum := int64(1)
+	divisorSum := big.NewInt(1)
 	for _, x := range PrimeFactors {
-		divisorSum *= int64((math.Pow(float64(x[0]), float64(x[1]+1)) - 1) / (float64(x[0]) - 1))
-		if divisorSum < 0 {
-			return -1, ErrIntegerOverflow
-		}
+		z0 := new(big.Int).Exp(big.NewInt(x[0]), big.NewInt(x[1]+1), nil)
+		z1 := new(big.Int).Mul(divisorSum, new(big.Int).Sub(z0, big.NewInt(1)))
+		z2 := new(big.Int).Sub(big.NewInt(x[0]), big.NewInt(1))
+		z3 := new(big.Int).Div(z1, z2)
+
+		divisorSum = z3
 	}
 
-	return divisorSum, nil
+	return *divisorSum, nil
+}
+
+func DivisorSumBig(n big.Int) (big.Int, error) {
+	if n.Sign() <= 0 {
+		return *big.NewInt(0), errors.New("value cannot be less than or equal to zero")
+	}
+
+	limit := new(big.Int).Sqrt(&n)
+	sum := big.NewInt(0)
+
+	for i := big.NewInt(1); i.Cmp(limit) < 1; i = new(big.Int).Add(i, big.NewInt(1)) {
+
+		if new(big.Int).Mod(&n, i).Cmp(big.NewInt(0)) == 0 {
+			sum = new(big.Int).Add(sum, i)
+			if new(big.Int).Mul(i, i).Cmp(&n) != 0 {
+				sum = new(big.Int).Add(sum, new(big.Int).Div(&n, i))
+			}
+		}
+
+	}
+
+	return *sum, nil
 }
--- riemann/riemann_suite_test.go
+++ riemann/riemann_suite_test.go
@@ -2,12 +2,16 @@ package riemann_test
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/ginkgo/v2/types"
 	. "github.com/onsi/gomega"
 )
 
 func TestDivisor(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Suite Tests")
+	RunSpecs(t, "Suite Tests", types.ReporterConfig{
+		SlowSpecThreshold: 100 * time.Millisecond,
+	})
 }
